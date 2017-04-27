103fd49c9fea5f4acbd4893a87fe3abd50c5f96f
Stephan Hügel <urschrei@gmail.com>
2017-04-27 13:52:01 +0100

    use different version-sort switch

--- ci/install.sh
+++ ci/install.sh
@@ -14,7 +14,7 @@ main() {
     local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
                        | cut -d/ -f3 \
                        | grep -E '^v[0.1.0-9.]+$' \
-                       | $sort --version-sort \
+                       | $sort -V \
                        | tail -n1)
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
