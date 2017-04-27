87192237e27a443db5eb47be364f544a05170b8d
Stephan Hügel <urschrei@gmail.com>
2017-04-27 13:24:03 +0100

    Add tag to travis build

--- ci/manylinux_rust.sh
+++ ci/manylinux_rust.sh
@@ -12,6 +12,14 @@ export PATH="$PATH:$HOME/.cargo/bin"
 export TRAVIS_RUST_VERSION=stable
 
 install_rustup() {
+
+    # This fetches latest stable release
+    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
+                       | cut -d/ -f3 \
+                       | grep -E '^v[0.1.0-9.]+$' \
+                       | $sort --version-sort \
+                       | tail -n1)
+
     curl -LSfs https://japaric.github.io/trust/install.sh | \
     sh -s -- \
        --force \
