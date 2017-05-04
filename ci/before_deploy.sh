cb5b2e5cbe6d3d3524bcbd7217e52e6618f52357
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2017-05-04 16:13:00 +0100

    Update pre-deploy

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -17,27 +17,23 @@ main() {
 
     test -f Cargo.lock || cargo generate-lockfile
 
-    # TODO Update this to build the artifacts that matter to you
-    cross rustc --target $TARGET --release
-
     if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-        for lib in target/$TARGET/release/*.so; do
-            strip -s $lib
-        done
+        # we've already built release binaries in Docker, so no-op
+        echo "Linux release artifacts built in manylinux1 Docker image"
     fi
     if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+        # TODO Update this to build the artifacts that matter to you
+        cross rustc --target $TARGET --release
         for lib in target/$TARGET/release/*.dylib; do
             strip -ur $lib
+        cp target/$TARGET/release/*.dylib $stage
+        # TODO Update this to package the right artifacts
+        cp -r target/$TARGET/release/*.dSYM $stage 2>/dev/null || :
+        cd $stage
+        tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+        cd $src
         done
     fi
-
-    # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/*.dylib $stage
-    cp -r target/$TARGET/release/*.dSYM $stage 2>/dev/null || :
-
-    cd $stage
-    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
-    cd $src
 
     rm -rf $stage
 }
