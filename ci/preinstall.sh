9a5a17a09c22676bc515d0a5ff9735155b818c3e
Stephan Hügel <urschrei@gmail.com>
2017-05-15 16:43:05 +0100

    Remove Python preinstall

--- ci/preinstall.sh
+++
@@ -1,9 +0,0 @@
-  if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    brew update
-    brew install python
-    pip install virtualenv
-    virtualenv venv -p python
-    source venv/bin/activate
-    pip install pip --upgrade
-    pip install travis-cargo
-fi
