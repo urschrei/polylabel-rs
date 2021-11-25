940c43cc0110461aa570b01af4ed2a96f8831fc5
Stephan Hügel <shugel@tcd.ie>
2021-11-25 16:53:20 +0000

    Generate headers on build

--- build.rs
+++ build.rs
@@ -2,11 +2,18 @@ extern crate cbindgen;
 
 use std::env;
 
-fn main() {
+fn write_headers() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let config = cbindgen::Config::from_file("cbindgen.toml").unwrap();
 
     cbindgen::generate_with_config(&crate_dir, config)
         .expect("Unable to generate bindings")
         .write_to_file("include/header.h");
+}
+
+fn main() {
+    let headers_enabled = env::var_os("CARGO_FEATURE_HEADERS").is_some();
+    if headers_enabled {
+        write_headers();
+    }
 }
